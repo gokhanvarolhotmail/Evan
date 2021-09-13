f46aa33d152636d6278272596a7ceedf5d9d0254
gokhanvarolhotmail <67670356+gokhanvarolhotmail@users.noreply.github.com>
2021-09-13 14:04:11 -0400

    gv

--- SSMSTemplates/Views/vDemoQIDXrefLayout.sql
+++ SSMSTemplates/Views/vDemoQIDXrefLayout.sql
@@ -3,32 +3,35 @@ GO
 CREATE OR ALTER VIEW [Definition].[vDemoQIDXrefLayout]
 AS
 SELECT
-    [Field #]
-  , [Fieldname] AS [FieldName]
-  , [Max Length] AS [Max_Length]
-  , [Data Type] AS [Data_Type]
-  , CASE WHEN [Fieldname] = 'Quantarium Internal PID' AND [Data Type] = 'INT' AND [Max Length] = 10 THEN 'BIGINT'
-        WHEN [Fieldname] = 'Location_ID' AND [Data Type] = 'varchar' AND [Max Length] = 50 THEN 'BIGINT'
-        WHEN [Fieldname] = 'Household_ID' AND [Data Type] = 'varchar' AND [Max Length] = 14 THEN 'BIGINT'
-        WHEN [Fieldname] = 'Individual_ID' AND [Data Type] = 'varchar' AND [Max Length] = 14 THEN 'BIGINT'
-        WHEN [Fieldname] = 'Location_ID' AND [Data Type] = 'varchar' AND [Max Length] = 14 THEN 'BIGINT'
-        WHEN [Data Type] IN ('varchar', 'char') THEN CONCAT('VARCHAR(', [Max Length], ')')
-        WHEN [Data Type] = 'Numeric' AND [Max Length] <= 4 THEN 'SMALLINT'
-        WHEN [Data Type] = 'Numeric' AND [Max Length] <= 9 THEN 'INT'
-        WHEN [Data Type] = 'Numeric' THEN 'BIGINT'
-        WHEN [Data Type] = 'INT' THEN 'INT'
-        WHEN [Data Type] = 'SMALLINT' THEN 'SMALLINT'
-        WHEN [Data Type] = 'BIGINT' THEN 'BIGINT'
-        WHEN [Data Type] = 'DATE' THEN 'DATE'
-        ELSE [Data Type]
-    END AS [ColumnDef]
-  , [Max Length]
-  , [Data Type]
-  , [Field Description]
-  , [Field Notes]
-  , [Version]
-  , [Change Type]
-FROM [Definition].[DemoQIDXrefLayout] ;
+    *
+  , CONCAT('CAST([Column] AS ', [k].[ColumnDef], ') AS ', QUOTENAME([k].[FieldName])) AS [Casted]
+FROM( SELECT
+          [Field #]
+        , [Fieldname] AS [FieldName]
+        , [Max Length] AS [Max_Length]
+        , [Data Type] AS [Data_Type]
+        , CASE WHEN [Fieldname] = 'Quantarium Internal PID' AND [Data Type] = 'INT' AND [Max Length] = 10 THEN 'BIGINT'
+              WHEN [Fieldname] = 'Location_ID' AND [Data Type] = 'varchar' AND [Max Length] = 50 THEN 'BIGINT'
+              WHEN [Fieldname] = 'Household_ID' AND [Data Type] = 'varchar' AND [Max Length] = 14 THEN 'BIGINT'
+              WHEN [Fieldname] = 'Individual_ID' AND [Data Type] = 'varchar' AND [Max Length] = 14 THEN 'BIGINT'
+              WHEN [Fieldname] = 'Location_ID' AND [Data Type] = 'varchar' AND [Max Length] = 14 THEN 'BIGINT'
+              WHEN [Data Type] IN ('varchar', 'char') THEN CONCAT('VARCHAR(', [Max Length], ')')
+              WHEN [Data Type] = 'Numeric' AND [Max Length] <= 4 THEN 'SMALLINT'
+              WHEN [Data Type] = 'Numeric' AND [Max Length] <= 9 THEN 'INT'
+              WHEN [Data Type] = 'Numeric' THEN 'BIGINT'
+              WHEN [Data Type] = 'INT' THEN 'INT'
+              WHEN [Data Type] = 'SMALLINT' THEN 'SMALLINT'
+              WHEN [Data Type] = 'BIGINT' THEN 'BIGINT'
+              WHEN [Data Type] = 'DATE' THEN 'DATE'
+              ELSE [Data Type]
+          END AS [ColumnDef]
+        , [Max Length]
+        , [Data Type]
+        , [Field Description]
+        , [Field Notes]
+        , [Version]
+        , [Change Type]
+      FROM [Definition].[DemoQIDXrefLayout] ) AS [k] ;
 GO
 SELECT CONCAT('DROP TABLE IF EXISTS [#DemoQIDXrefLayout]
 GO
--- SSMSTemplates/Views/vOpenLien.sql
+++ SSMSTemplates/Views/vOpenLien.sql
@@ -3,37 +3,42 @@ GO
 CREATE OR ALTER VIEW [Definition].[vOpenLien]
 AS
 SELECT
-    [Field] AS [Field #]
-  , [Field_Display_Name] AS [FieldName]
-  , [Max_Length]
-  , [Data_Type]
-  , CASE WHEN [Field_Display_Name] = 'Quantarium Internal PID' AND [Data_Type] = 'INT' AND [Max_Length] = 10 THEN 'BIGINT'
-        WHEN [Field_Display_Name] = 'Location_ID' AND [Data_Type] = 'varchar' AND [Max_Length] = 50 THEN 'BIGINT'
-        WHEN [Field_Display_Name] = 'Household_ID' AND [Data_Type] = 'varchar' AND [Max_Length] = 14 THEN 'BIGINT'
-        WHEN [Field_Display_Name] = 'Individual_ID' AND [Data_Type] = 'varchar' AND [Max_Length] = 14 THEN 'BIGINT'
-        WHEN [Field_Display_Name] = 'Location_ID' AND [Data_Type] = 'varchar' AND [Max_Length] = 14 THEN 'BIGINT'
-        WHEN [Data_Type] IN ('varchar', 'char') THEN CONCAT('VARCHAR(', [Max_Length], ')')
-        WHEN [Data_Type] = 'Numeric' AND [Max_Length] <= 4 THEN 'SMALLINT'
-        WHEN [Data_Type] = 'Numeric' AND [Max_Length] <= 9 THEN 'INT'
-        WHEN [Data_Type] = 'Numeric' THEN 'BIGINT'
-        WHEN [Data_Type] = 'INT' THEN 'INT'
-        WHEN [Data_Type] = 'SMALLINT' THEN 'SMALLINT'
-        WHEN [Data_Type] = 'BIGINT' THEN 'BIGINT'
-        WHEN [Data_Type] = 'DATE' THEN 'DATE'
-        ELSE [Data_Type]
-    END AS [ColumnDef]
-  , [Field]
-  , [Data_Category]
-  , [Field_Display_Name]
-  , [Header_Name_in_Delivered_File]
-  , [Data_Format]
-  , [CodeXlate_Field]
-  , [Field_Description]
-  , [Version]
-  , [Change_Type]
-FROM [Definition].[OpenLien] ;
+    *
+  , CASE WHEN [k].[Data_Format] = 'YYYYMMDD' THEN CONCAT('CAST([Column] AS DATE) AS ', QUOTENAME([k].[Field_Display_Name]), ',')
+        WHEN [k].[Data_Format] = 'MMDDYYYY' THEN CONCAT('CAST(RIGHT([Column],4) + LEFT([Column],4) AS DATE) AS ', QUOTENAME([k].[Field_Display_Name]), ',')
+        ELSE CONCAT('CAST([Column] AS ', [k].[ColumnDef], ') AS ', QUOTENAME([k].[Field_Display_Name]))
+    END AS [Casted]
+FROM( SELECT
+          [Field] AS [Field #]
+        , [Field_Display_Name] AS [FieldName]
+        , [Max_Length]
+        , [Data_Type]
+        , CASE WHEN [Field_Display_Name] = 'Quantarium Internal PID' AND [Data_Type] = 'INT' AND [Max_Length] = 10 THEN 'BIGINT'
+              WHEN [Field_Display_Name] = 'Location_ID' AND [Data_Type] = 'varchar' AND [Max_Length] = 50 THEN 'BIGINT'
+              WHEN [Field_Display_Name] = 'Household_ID' AND [Data_Type] = 'varchar' AND [Max_Length] = 14 THEN 'BIGINT'
+              WHEN [Field_Display_Name] = 'Individual_ID' AND [Data_Type] = 'varchar' AND [Max_Length] = 14 THEN 'BIGINT'
+              WHEN [Field_Display_Name] = 'Location_ID' AND [Data_Type] = 'varchar' AND [Max_Length] = 14 THEN 'BIGINT'
+              WHEN [Data_Type] IN ('varchar', 'char') THEN CONCAT('VARCHAR(', [Max_Length], ')')
+              WHEN [Data_Type] = 'Numeric' AND [Max_Length] <= 4 THEN 'SMALLINT'
+              WHEN [Data_Type] = 'Numeric' AND [Max_Length] <= 9 THEN 'INT'
+              WHEN [Data_Type] = 'Numeric' THEN 'BIGINT'
+              WHEN [Data_Type] = 'INT' THEN 'INT'
+              WHEN [Data_Type] = 'SMALLINT' THEN 'SMALLINT'
+              WHEN [Data_Type] = 'BIGINT' THEN 'BIGINT'
+              WHEN [Data_Type] = 'DATE' THEN 'DATE'
+              ELSE [Data_Type]
+          END AS [ColumnDef]
+        , [Field]
+        , [Data_Category]
+        , [Field_Display_Name]
+        , [Header_Name_in_Delivered_File]
+        , [Data_Format]
+        , [CodeXlate_Field]
+        , [Field_Description]
+        , [Version]
+        , [Change_Type]
+      FROM [Definition].[OpenLien] ) AS [k] ;
 GO
-
 SELECT CONCAT('DROP TABLE IF EXISTS [#OpenLien]
 GO
 CREATE TABLE [#OpenLien]
